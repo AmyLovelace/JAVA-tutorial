3a5b5bbbe91f9950595ae894f2030d4f27de1f2e
amanecer <amanecer.luz.cabrera@gmail.com>
2023-05-18 19:31:03 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        /* auto suma*/
+        int a = 3;
+        a += 2;
+        /*  auto multiplicacion */
+        a *= 5;
+        /*auto division*/
+        a /= 5;
+        System.out.println(a);
+
+
+
+    }
+
+
+
+}
